d67e881ad29e43932f43f59b78f27a1bad97b830
TomaszCwik <34006184+TomaszCwik@users.noreply.github.com>
2019-06-14 22:35:14 +0200

    Dodanie Czasu i mety

---
+++ GK_Laboratorium/Assets/Level1/Scripts/Timer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text timerText;
+    public float secondsCount;
+    public int minuteCount;
+    private bool finnished =false;
+    void Update()
+    {
+        UpdateTimerUI();
+    }
+    //call this on update
+    public void UpdateTimerUI()
+    {
+        //set timer UI
+        if (finnished)
+            return;
+        secondsCount += Time.deltaTime;
+        timerText.text = minuteCount + "m:" + (int)secondsCount + "s";
+        if (secondsCount >= 60)
+        {
+            minuteCount++;
+            secondsCount = 0;
+        }
+         
+    }
+    public void Finnish()
+    {
+        finnished = true;
+        timerText.color = Color.yellow;
+    }
+}
---
+++ GK_Laboratorium/Assets/Level1/Scripts/WinBox.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinBox : MonoBehaviour
+{
+private void onTriggerEnter (Collider other)
+    {
+        if(GameObject.Find("Gracz"))
+        {
+           SendMessage("Finnish");
+        }
+    }
+}
